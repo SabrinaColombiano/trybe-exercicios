d597a615150a993c6e4f6b3b9efabf6763b755fc
sabrina <sabrina.colombiano@yahoo.com>
2022-11-26 23:00:27 -0300

    exercicioc for e arrays

--- fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercicio-01.js
+++ fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercicio-01.js
@@ -1,6 +1,6 @@
 let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for (let index = 0; index < groceryList.length; index++) {
-  const element = groceryList[index];
+for (let index = 0; index < groceryList.length; index += 1) {
+  let element = groceryList[index];
   console.log(element);
 }
---
+++ fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercicio-02.js
@@ -0,0 +1,12 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+//for(let index of names) {
+//  console.log(index);
+//}
+
+
+
+
+for (let index of names) {
+    console.log(index);
+}
