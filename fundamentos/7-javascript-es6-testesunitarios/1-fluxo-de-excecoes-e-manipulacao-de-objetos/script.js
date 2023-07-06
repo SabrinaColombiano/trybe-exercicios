7bcc78bbd3e879000dec74a13361982e34bee6d7
Sabrina Colombiano <sabrina.colombiano@yahoo.com>
2023-07-06 13:45:07 -0300

    casa de cambio

---
+++ fundamentos/7-javascript-es6-testesunitarios/1-fluxo-de-excecoes-e-manipulacao-de-objetos/script.js
@@ -0,0 +1,68 @@
+// Primeira parte
+const promo = [
+    {product: 'bola de beach tennis', price: 29.99},
+    {product: 'mochila de trilha 20 litros', price: 120.99},
+    {product: 'capacete de ciclista', price: 180.59},
+    {product: 'toalha compacta de secagem', price: 39.99},
+    {product: 'kit 5 camisetas Dry Fit', price: 99.99},
+    {product: 'caneleiras de peso - 3kg (par)', price: 44.99},
+    {product: 'bola de ginástica e pilates', price: 149.59},
+    {product: 'óculos de natação', price: 18.99},
+    {product: 'capa protetora de prancha de surf', price: 89.59},
+    {product: 'bola de basquete', price: 39.99},
+  ];
+
+  const button = document.querySelector('#send-button');
+
+button.addEventListener('click', () => {
+  const name = document.querySelector('#name-id').value;
+  const number = document.querySelector('#number-id').value;
+
+  showPromo(name, number);
+});
+
+
+const showPromo = (name, number) => {
+    const firstText = document.querySelector('#text-initial');
+    const secondText = document.querySelector('#text-final');
+    
+    try {
+      checkName(name);
+      checkNumber(parseInt(number));
+      checkValidRange(number);
+      checkPromo(number);
+
+      const productObject = checkPromo(number);
+      firstText.innerHTML = `Boas-vindas, ${name}!`;
+      secondText.innerHTML = `A promoção do dia é: 
+        ${productObject.product} no valor de R$ ${productObject.price}`;
+    } catch(err) {
+      secondText.innerHTML = err.message;
+    }
+  }
+
+// Segunda parte
+const checkNumber = (number) => {
+  return promo[number - 1];
+}
+
+const checkName = (name) => {
+    let letters = /[aA-zZ]+/;
+    
+    if (!name.match(letters)) {
+      throw new Error('É necessário digitar um nome válido');
+    }
+  }
+// Terceira parte
+const checkPromo = (number) => {
+    for (let index = 0; index < promo.length; index += 1) {
+      if (number - 1 === index) {
+        return promo[index];
+      }
+    }
+  }
+  const checkValidRange = (number) => {
+    if (number < 1 || number > 10) {
+      throw new Error('É necessário digitar um número entre 1 e 10');
+    }
+  }
